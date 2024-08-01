2cc1f8c3473b106b38468e261d32f41613f355dc
Sakaisha <aishasakkary@outlook.com>
2024-08-01 15:23:13 +0300

    GTK Window adjusted from 600x600 to 300x350 for better GUI experience.

--- main.c
+++ main.c
@@ -100,7 +100,7 @@ int main(int argc, char *argv[]) {
 
     window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
     gtk_window_set_title(GTK_WINDOW(window), "Connect 4");
-    gtk_window_set_default_size(GTK_WINDOW(window), 800, 600);
+    gtk_window_set_default_size(GTK_WINDOW(window), 300, 350);
     g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
 
     vbox = gtk_box_new(GTK_ORIENTATION_VERTICAL, 5);
